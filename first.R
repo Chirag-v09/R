83ee233db993bf7318fa582883f29fc62f5d21d1
Chirag Verma <cv091998@gmail.com>
2020-08-12 10:23:58 +0530

    Add files via upload

---
+++ first.R
@@ -0,0 +1,60 @@
+3+2
+5+9
+
+#assignment
+a<-b<-5
+c = 5
+assign('d', 7)
+e = -3.7
+f = 'hello world'
+nchar(f) # Show no of characters
+
+#see datatype
+class(a)
+class(e)
+
+#dates
+dates <- as.Date("2019-08-09")
+class(dates)
+
+#logical oprations
+7 < 9
+7 == 7
+7 == 9
+'chirag' == 'chirag'
+'hello' == 'world'
+
+
+#vectors
+v1 <- c(1,2,3,4,5)
+v2 <- c('hello', 'world', 'first')
+v1*2
+sqrt(v1)
+v1^3
+
+v1[1:5]
+
+c1 <- 1:10
+c1
+c2 <- 11:20
+c2
+
+c1+c2
+c1*c2
+
+c3 <- c1^c2
+c3[3]
+
+2^12
+3^13
+
+vec <- -3:-10
+
+b1 <- 1:5
+b2 <- 1:3
+b1+b2
+
+vector <- 1:10
+any(vector<5)
+all(vector<5)
+vector<5
